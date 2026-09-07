567a8f9a4a836922cee01d264f657c549ffc580a
snehpnp <snehpnp@gmail.com>
2026-09-07 10:51:24 +0530

    dsdsd

--- frontend/src/pages/TemplateEditor.tsx
+++ frontend/src/pages/TemplateEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import {
   getTemplateById,
